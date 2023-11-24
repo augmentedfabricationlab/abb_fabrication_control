cf5cbb5c4854b60d13a3948d43b3d55286dba25e
Ema Krakovska <ema@krakovsky.sk>
2023-11-24 12:08:55 +0100

    feedback level

--- src/abb_fabrication_control/fabrication/abbtask.py
+++ src/abb_fabrication_control/fabrication/abbtask.py
@@ -5,28 +5,27 @@ from compas.geometry import Frame
 import compas_rrc as rrc
 
 class ABBTask(Task):
-    def __init__(self, robot, *args, **kwargs):
-        super(ABBTask, self).__init__(*args, **kwargs)
+    def __init__(self, robot, key):
+        super(ABBTask, self).__init__(key)
         self.robot = robot
-        self.frame = Frame.WorldXY()
 
     def content(self):
         ''' Script to execute within the run statement '''
         
         ### Saving the final command to self.future (ensure feedback_level>0)
-        future = abbfc.commands.move_to_robtarget(self.robot, frame=self.frame,
-                                                  cart=0, speed=250, zone=rrc.Zone.FINE
+        future = abbfc.commands.move_to_robtarget(self.robot, frame=Frame.worldXY(),
+                                                  cart=0, speed=250, zone=rrc.Zone.FINE,
                                                   feedback_level=1)
         return future
 
     def run(self, stop_thread):
         future = self.content()
         if future is None:
-            raise ValueError("self.future is None, please set a command to wait for")
+            raise ValueError("future is None, please set a command to wait for")
         while not future.done:
             if stop_thread():
                 self.log("Forced to stop...")
-                self.robot.abb_client.send(rrc.Stop())
+                #self.robot.abb_client.send(rrc.Stop())
                 self.is_running = False
                 self.is_completed = False
                 break   
@@ -36,4 +35,35 @@ class ABBTask(Task):
         
         
         
-    
+# class ABBTask2(Task):
+#     def __init__(self, robot, *args, **kwargs):
+#         super(ABBTask, self).__init__(*args, **kwargs)
+#         self.robot = robot
+#         self.frame = Frame.WorldXY()
+
+#     def run(self, stop_thread, future=None):
+#         '''overwrite and set variable future'''
+#         if future is None:
+#             raise ValueError("self.future is None, please set a command to wait for")
+#         while not future.done:
+#             if stop_thread():
+#                 self.log("Forced to stop...")
+#                 self.robot.abb_client.send(rrc.Stop())
+#                 self.is_running = False
+#                 self.is_completed = False
+#                 break   
+#         else:
+#             self.is_completed = True
+#             return True
+        
+# class SetToolTask(ABBTask):
+#     def __init__(self, robot, tool_name, key=None):
+#         super(SetToolTask, self).__init__(key)
+#         self.robot = robot
+#         self.tool_name = tool_name
+        
+#     def run(self, stop_thread):
+#         # Sets a tool to the robot
+#         future = self.robot.abb_client.send(rrc.SetTool(self.tool_name), feedback_level=1)
+#         self.log("{} is set".format(self.tool_name))
+#         return super(SetToolTask, self).run(stop_thread, future)
