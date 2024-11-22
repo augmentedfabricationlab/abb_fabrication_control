aeeb25551f14eed0bbc543b37da6a7d5b523c1dd
JannaVollrath <156815650+JannaVollrath@users.noreply.github.com>
2024-11-22 17:51:33 +0100

    Update __init__.py

--- src/abb_fabrication_control/commands/__init__.py
+++ src/abb_fabrication_control/commands/__init__.py
@@ -2,7 +2,7 @@
 from .basics import print_text, set_tool, set_tool_attached
 from .motion import move_to_frame, move_to_robtarget, move_to_joints
 from .position import get_frame, get_robtarget, get_joints
-from .input_output import set_digital_out, open_gripper, close_gripper
+from .input_output import set_digital_out, open_gripper, close_gripper, enable_gun, disable_gun, trigger_gun
 
 __all__ = ["print_text",
            "set_tool",
