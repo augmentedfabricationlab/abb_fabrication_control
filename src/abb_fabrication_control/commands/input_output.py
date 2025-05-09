42abdd46bfa3bb841bba3ce2e53bf48284293f73
JannaVollrath <156815650+JannaVollrath@users.noreply.github.com>
2025-05-09 11:04:34 +0200

    update input for nailgun trigger

--- src/abb_fabrication_control/commands/input_output.py
+++ src/abb_fabrication_control/commands/input_output.py
@@ -25,13 +25,13 @@ def open_gripper(robot, send_and_wait=False):
     set_digital_out(robot, 'Ausgang_100_5', output_state=0, send_and_wait=send_and_wait)
     return robot.abb_client.send(rrc.WaitTime(0.5))
 
-def enable_gun(robot, send_and_wait=False):
+#def enable_gun(robot, send_and_wait=False):
     """ Send signal to enable the gun
     """
     set_digital_out(robot, 'Ausgang_100_1', output_state=1, send_and_wait=send_and_wait)
     return robot.abb_client.send(rrc.WaitTime(0.5))
 
-def disable_gun(robot, send_and_wait=False):
+#def disable_gun(robot, send_and_wait=False):
     """ Send signal to disable the gun
     """
     set_digital_out(robot, 'Ausgang_100_1', output_state=0, send_and_wait=send_and_wait)
@@ -40,9 +40,9 @@ def disable_gun(robot, send_and_wait=False):
 def trigger_gun(robot, send_and_wait=False):
     """ Send signal to trigger the gun
     """
-    set_digital_out(robot, 'Ausgang_100_4', output_state=1, send_and_wait=send_and_wait)
+    set_digital_out(robot, 'Ausgang_100_1', output_state=1, send_and_wait=send_and_wait)
     robot.abb_client.send(rrc.WaitTime(1))
-    set_digital_out(robot, 'Ausgang_100_4', output_state=0, send_and_wait=send_and_wait)
+    set_digital_out(robot, 'Ausgang_100_1', output_state=0, send_and_wait=send_and_wait)
     return robot.abb_client.send(rrc.WaitTime(0.5))
 
 
