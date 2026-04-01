107af9efb4dbb71cd2768e73c6e50e33c130a852
Ema Krakovska <ema@krakovsky.sk>
2026-04-01 11:52:59 +0200

    removed non-general methods

--- src/abb_fabrication_control/commands/input_output.py
+++ src/abb_fabrication_control/commands/input_output.py
@@ -11,62 +11,9 @@ def set_digital_out(robot, output_name, output_state=0, send_and_wait=False):
         # Send command to the ABB controller without waiting for feedback
         return robot.abb_client.send(rrc.SetDigital(output_name,output_state))
 
-def close_gripper(robot, send_and_wait=False):
-    """ Send signal to open the gripper
-    """
-    set_digital_out(robot, 'Ausgang_100_3', output_state=0, send_and_wait=send_and_wait)
-    set_digital_out(robot, 'Ausgang_100_5', output_state=1, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.5))
-
-def open_gripper(robot, send_and_wait=False):
-    """ Send signal to open the gripper
-    """
-    set_digital_out(robot, 'Ausgang_100_3', output_state=1, send_and_wait=send_and_wait)
-    set_digital_out(robot, 'Ausgang_100_5', output_state=0, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.5))
-
-def enable_gun(robot, send_and_wait=False):
-    """ Send signal to enable the gun
-    """
-    set_digital_out(robot, 'Ausgang_100_1', output_state=1, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.5))
-
-def disable_gun(robot, send_and_wait=False):
-    """ Send signal to disable the gun
-    """
-    set_digital_out(robot, 'Ausgang_100_1', output_state=0, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.5))
-
-def trigger_gun(robot, send_and_wait=False):
-    """ Send signal to trigger the gun
-    """
-    set_digital_out(robot, 'Ausgang_100_1', output_state=1, send_and_wait=send_and_wait)
-    robot.abb_client.send(rrc.WaitTime(1))
-    set_digital_out(robot, 'Ausgang_100_1', output_state=0, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.5))
 
 
-def open_extruder(robot, send_and_wait=False):
-    """ Send signal to open the extruder
-    """
-    set_digital_out(robot, 'Ausgang_100_4', output_state=0, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.5))
 
-def close_extruder(robot, send_and_wait=False):
-    """ Send signal to close the extruder
-    """
-    set_digital_out(robot, 'Ausgang_100_3', output_state=1, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.5))
-
-def single_stamp(robot, send_and_wait=False):
-    """ Send signal for a single stamp motion
-    """
-    set_digital_out(robot, 'Ausgang_100_2', output_state=1, send_and_wait=send_and_wait)
-    set_digital_out(robot, 'Ausgang_100_3', output_state=0, send_and_wait=send_and_wait)
-    robot.abb_client.send(rrc.WaitTime(0.2))
-    set_digital_out(robot, 'Ausgang_100_2', output_state=0, send_and_wait=send_and_wait)
-    set_digital_out(robot, 'Ausgang_100_3', output_state=1, send_and_wait=send_and_wait)
-    return robot.abb_client.send(rrc.WaitTime(0.2))
 
 
 
--- src/abb_fabrication_control/tasks/abbtask.py
+++ src/abb_fabrication_control/tasks/abbtask.py
@@ -1,6 +1,6 @@
 from fabrication_manager import Task
 import abb_fabrication_control as abbfc
-from compas.geometry import Frame, Translation, Vector
+from compas.geometry import Frame
 import compas_rrc as rrc
 
 class ABBTask(Task):
@@ -10,7 +10,7 @@ class ABBTask(Task):
 
     def content(self):
         ''' Script to execute within the run statement '''
-        
+
         ### Saving the final command to self.future (ensure feedback_level>0)
         future = abbfc.commands.motion.move_to_robtarget(self.robot, frame=Frame.worldXY(),
                                                   cart=0, speed=250, zone=rrc.Zone.FINE,
@@ -27,89 +27,7 @@ class ABBTask(Task):
                 #self.robot.abb_client.send(rrc.Stop())
                 self.is_running = False
                 self.is_completed = False
-                break   
+                break
         else:
             self.is_completed = True
             return True
-        
-class StartConfigurationTask(ABBTask):
-    def __init__(self, robot, configuration, key=None):
-        super(StartConfigurationTask, self).__init__(robot, key)
-        self.configuration = configuration
-        
-    def content(self):
-        # Moves the robot to start configuration
-        abbfc.commands.motion.move_to_joints(self.robot, self.configuration, send_and_wait = True)
-        self.log("robot moved to start configuration")  
-        future = self.robot.abb_client.send(rrc.PrintText("Moved to startconfiguration done", feedback_level=1))      
-        return future
-            
-class SetToolTask(ABBTask):
-    def __init__(self, robot, tool_name, key=None):
-        super(SetToolTask, self).__init__(robot, key)
-        self.tool_name = tool_name
-        
-    def content(self):
-        # Sets a tool to the robot
-        self.robot.abb_client.send(rrc.SetTool(self.tool_name))
-        self.log("{} is set".format(self.tool_name))
-        future = self.robot.abb_client.send(rrc.PrintText("Setting tool done", feedback_level=1))
-        return future        
-         
-class PickTask(ABBTask):
-    def __init__(self, robot, pick_frame, key=None):
-        super(PickTask, self).__init__(robot, key)
-        self.pick_frame = pick_frame
-        
-    def content(self):
-        T = Translation.from_vector(Vector(0,0,0.2)) 
-        
-        # Moves to approach frame
-        approach_frame = self.pick_frame.transformed(T)
-        abbfc.commands.motion.move_to_frame(self.robot, approach_frame)
-        self.log("robot moved to approach frame") 
-
-        # Moves to pick frame
-        abbfc.commands.motion.move_to_frame(self.robot, self.pick_frame)
-        self.log("robot moved to pick frame")  
-
-        # Closes the gripper
-        abbfc.commands.input_output.close_gripper(self.robot)
-        self.log("gripper closed")
-
-        # Moves to approach frame
-        approach_frame = self.pick_frame.transformed(T)
-        abbfc.commands.motion.move_to_frame(self.robot, approach_frame)
-        self.log("robot moved to approach frame") 
-
-        future = self.robot.abb_client.send(rrc.PrintText("Picking brick done", feedback_level=1))
-        return future 
-    
-class PlaceTask(ABBTask):
-    def __init__(self, robot, place_frame, key=None):
-        super(PlaceTask, self).__init__(robot, key)
-        self.place_frame = place_frame
-        
-    def content(self):
-        T = Translation.from_vector(Vector(0,0,0.2)) 
-        
-        # Moves to approach frame
-        approach_frame = self.place_frame.transformed(T)
-        abbfc.commands.motion.move_to_frame(self.robot, approach_frame)
-        self.log("robot moved to approach frame") 
-
-        # Moves to place frame
-        abbfc.commands.motion.move_to_frame(self.robot, self.place_frame)
-        self.log("robot moved to pick frame")  
-
-        # Opens the gripper
-        abbfc.commands.input_output.open_gripper(self.robot)
-        self.log("gripper closed")
-
-        # Moves to approach frame
-        approach_frame = self.pick_frame.transformed(T)
-        abbfc.commands.motion.move_to_frame(self.robot, approach_frame)
-        self.log("robot moved to approach frame") 
-
-        future = self.robot.abb_client.send(rrc.PrintText("Placing brick done", feedback_level=1))
-        return future 
